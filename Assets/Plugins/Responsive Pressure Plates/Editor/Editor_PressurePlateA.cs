39f92e2444badce745975f4155823b39481eb61a
Baoze <2635040197@qq.com>
2022-07-02 19:17:22 +0800

    V0,6a

---
+++ Assets/Plugins/Responsive Pressure Plates/Editor/Editor_PressurePlateA.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CanEditMultipleObjects]
+[CustomEditor(typeof(PressurePlateScript))]
+public class Editor_PressurePlateA : Editor
+{
+
+    PressurePlateScript editorTarget;
+
+    SerializedProperty index;
+    SerializedProperty isInteractive;
+    SerializedProperty TagCollisionEnter;
+    SerializedProperty TagCollisionExit;
+    SerializedProperty TagCollisionStay;
+    SerializedProperty ShowCollisionEnterEvent;
+    SerializedProperty ShowCollisionStayEvent;
+    SerializedProperty ShowCollisionExitEvent;
+    SerializedProperty DisableAudio;
+    SerializedProperty DisableAnimations;
+
+    void OnEnable()
+    {
+        editorTarget = target as PressurePlateScript;
+        index = serializedObject.FindProperty("index");
+        isInteractive = serializedObject.FindProperty("isInteractive");
+        TagCollisionEnter = serializedObject.FindProperty("TagCollisionEnter");
+        TagCollisionExit = serializedObject.FindProperty("TagCollisionExit");
+        TagCollisionStay = serializedObject.FindProperty("TagCollisionStay");
+        ShowCollisionEnterEvent = serializedObject.FindProperty("ShowCollisionEnterEvent");
+        ShowCollisionStayEvent = serializedObject.FindProperty("ShowCollisionStayEvent");
+        ShowCollisionExitEvent = serializedObject.FindProperty("ShowCollisionExitEvent");
+        DisableAudio = serializedObject.FindProperty("DisableAudio");
+        DisableAnimations = serializedObject.FindProperty("DisableAnimations");
+    }
+
+    public override void OnInspectorGUI()
+    {
+        serializedObject.Update();
+
+        EditorGUILayout.Space();
+        isInteractive.boolValue = EditorGUILayout.Toggle("Is Interactive: ", isInteractive.boolValue);
+        EditorGUILayout.Space();
+        if (isInteractive.boolValue)
+        {
+            EditorGUILayout.LabelField("Responsivity", EditorStyles.boldLabel);
+            EditorGUILayout.BeginHorizontal();
+            EditorGUILayout.LabelField("Interact with tag: ", GUILayout.MaxWidth(120));
+            index.intValue = EditorGUILayout.Popup(index.intValue, editorTarget.options);
+            editorTarget.index = index.intValue;
+            EditorGUILayout.EndHorizontal();
+            EditorGUILayout.Space();
+            EditorGUILayout.Space();
+
+            EditorGUILayout.BeginHorizontal();
+            EditorGUILayout.LabelField("On Tag Collision Enter:", GUILayout.MaxWidth(167));
+            ShowCollisionEnterEvent.boolValue = EditorGUILayout.Toggle(ShowCollisionEnterEvent.boolValue);
+            EditorGUILayout.EndHorizontal();
+            if (ShowCollisionEnterEvent.boolValue)
+            {
+                EditorGUILayout.PropertyField(TagCollisionEnter);
+            }
+            EditorGUILayout.Space();
+
+            EditorGUILayout.BeginHorizontal();
+            EditorGUILayout.LabelField("On Tag Collision Stay:", GUILayout.MaxWidth(167));
+            ShowCollisionStayEvent.boolValue = EditorGUILayout.Toggle(ShowCollisionStayEvent.boolValue);
+            EditorGUILayout.EndHorizontal();
+            if (ShowCollisionStayEvent.boolValue)
+            {
+                EditorGUILayout.PropertyField(TagCollisionStay);
+            }
+            EditorGUILayout.Space();
+
+            EditorGUILayout.BeginHorizontal();
+            EditorGUILayout.LabelField("On Tag Collision Exit:", GUILayout.MaxWidth(167));
+            ShowCollisionExitEvent.boolValue = EditorGUILayout.Toggle(ShowCollisionExitEvent.boolValue);
+            EditorGUILayout.EndHorizontal();
+            if (ShowCollisionExitEvent.boolValue)
+            {
+                EditorGUILayout.PropertyField(TagCollisionExit);
+            }
+
+            EditorGUILayout.Space();
+            EditorGUILayout.Space();
+            EditorGUILayout.LabelField("Audio", EditorStyles.boldLabel);
+            EditorGUILayout.PropertyField(DisableAudio);
+            EditorGUILayout.Space();
+
+            EditorGUILayout.LabelField("Animations", EditorStyles.boldLabel);
+            EditorGUILayout.PropertyField(DisableAnimations);
+        }
+
+        serializedObject.ApplyModifiedProperties();
+    }
+
+}
---
+++ Assets/Plugins/Responsive Pressure Plates/Scripts/PressurePlateScript.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Events;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class PressurePlateScript : MonoBehaviour
+{
+    [HideInInspector]
+    public string[] options;
+    [HideInInspector]
+    public int index;
+    [HideInInspector]
+    public bool isInteractive;
+    [HideInInspector]
+    public UnityEvent TagCollisionEnter;
+    [HideInInspector]
+    public UnityEvent TagCollisionStay;
+    [HideInInspector]
+    public UnityEvent TagCollisionExit;
+    [HideInInspector]
+    public bool ShowCollisionEnterEvent;
+    [HideInInspector]
+    public bool ShowCollisionStayEvent;
+    [HideInInspector]
+    public bool ShowCollisionExitEvent;
+    [HideInInspector]
+    public bool DisableAudio;
+    [HideInInspector]
+    public bool DisableAnimations;
+
+    Animator anim;
+    AudioSource source;
+
+    private void Awake()
+    {
+        anim = this.GetComponent<Animator>();
+        source = this.GetComponent<AudioSource>();
+    }
+
+    private void Update()
+    {
+        options = UnityEditorInternal.InternalEditorUtility.tags; // Get all available tags from the Editor
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        
+        if(collision.gameObject.tag == options[index]) // If colliding gameObject has the same tag as the selected tag of this script
+        {
+
+            if (!DisableAudio)
+            {
+                source.pitch = 2f;
+                source.Play();
+            }
+
+            if (!DisableAudio)
+            {
+                anim.Play("PressurePlate_Press");
+            }
+
+            TagCollisionEnter.Invoke(); // Run all functions assigned to the 'On Tag Collision Enter' UnityEvent
+
+        }
+
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+
+        if (collision.gameObject.tag == "Object")
+        {
+
+            if (!DisableAudio)
+            {
+                source.pitch = 1.8f;
+                source.Play();
+            }
+
+            if (!DisableAnimations)
+            {
+                anim.Play("PressurePlate_Release");
+            }
+
+            TagCollisionExit.Invoke();
+        }
+
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        TagCollisionStay.Invoke();
+    }
+
+}
