6b927fd15e14e59adb4fe7e4bd18cfa6f97703c2
iChiv.aka.Zv N <ichiv.0zv@gmail.com>
2022-07-06 03:51:28 +0800

    Fix Camera position and picked up.

--- Assets/Scripts/PickUpCamera.cs
+++ Assets/Scripts/PickUpCamera.cs
@@ -7,30 +7,47 @@ public class PickUpCamera : MonoBehaviour
     GameObject MainCam;
     GameObject Player;
     GameObject CamForPlayer;
+    GameObject Canvas;
+    GameObject Check;
     RaycastHit hit;
 
     // Start is called before the first frame update
     void Start()
     {
         MainCam = GameObject.Find("Main Camera");
-        Player = MainCam.transform.parent.gameObject;
-        CamForPlayer = Player.transform.GetChild(3).gameObject;
+        //Player = MainCam.transform.parent.gameObject;
+        CamForPlayer = MainCam.transform.GetChild(0).gameObject;
+        Canvas = GameObject.Find("Canvas");
+        Check = Canvas.transform.GetChild(1).gameObject;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        if (Input.GetKeyDown(KeyCode.E))
+        if(Physics.Raycast(transform.position, transform.forward, out hit , 10))
         {
-            if (Physics.Raycast(transform.position, transform.forward, out hit, 10))
+            if(hit.collider.tag == "camObj")
             {
-                if(hit.collider.tag == "camObj")
+                Check.gameObject.SetActive(true);
+                if (Input.GetKeyDown(KeyCode.E))
                 {
                     hit.collider.gameObject.SetActive(false);
                     CamForPlayer.gameObject.SetActive(true);
+                    Check.gameObject.SetActive(false);
                 }
             }
+           
         }
-
+        //if (Input.GetKeyDown(KeyCode.E))
+        //{
+        //    if (Physics.Raycast(transform.position, transform.forward, out hit, 100))
+        //    {
+        //        if(hit.collider.tag == "camObj")
+        //        {
+        //            hit.collider.gameObject.SetActive(false);
+        //            CamForPlayer.gameObject.SetActive(true);
+         //       }
+           // }
+        //}
     }
 }
